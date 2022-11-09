43b9ea5dd648f7c2034439f2492a0cfff2c24827
doddatpivotal <dpfeffer@vmware.com>
2022-11-09 08:17:20 -0500

    Adds developer namespaces and minor updates

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -9,16 +9,5 @@ fi
 
 CLUSTER_NAME=$1
 
-kubectl config use-context tap-$CLUSTER_NAME
 kapp deploy -a platform-svcs-gitops -f <(ytt -f common/gitops -f clusters/$CLUSTER_NAME/gitops --data-values-file=$PARAMS_YAML) -y -n default
-
-if [ "$CLUSTER_NAME" = "view" ]; then
-  ./scripts/enable-metadata-store-access.sh
-else
-  if [ "$CLUSTER_NAME" = "full" ]; then
-    echo "full cluster profile, skipping post processing "
-  else
-    ./scripts/enable-tap-gui-visibility.sh $CLUSTER_NAME
-  fi
-fi
 
